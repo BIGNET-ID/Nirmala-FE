08fb2d091241102bcc8361eeab5b0310018d19f3
Eka Bayu P <eka.bayu@bignet.id>
2026-08-28 18:03:11 +0700

    Update route.js

--- src/app/api/owm/[...tile]/route.js
+++ src/app/api/owm/[...tile]/route.js
@@ -11,7 +11,8 @@
 
 export const dynamic = 'force-dynamic';
 
-const KEY = process.env.OPENWEATHER_API_KEY || '';
+const KEY = '9458ca69741cda706dec8092eb22610e' || '';
+// const KEY = process.env.OPENWEATHER_API_KEY || '';
 const LAYERS = new Set(['precipitation_new', 'clouds_new', 'wind_new', 'temp_new', 'pressure_new']);
 
 export async function GET(request, ctx) {
